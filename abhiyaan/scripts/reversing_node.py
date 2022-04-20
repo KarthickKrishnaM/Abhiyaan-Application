b5ec0614383092c25f338aef002593e8ac847e3e
Karthick Krishna M <73303030+KarthickKrishnaM@users.noreply.github.com>
2022-04-20 21:22:53 +0530

    Update reversing_node.py

--- abhiyaan/scripts/reversing_node.py
+++ abhiyaan/scripts/reversing_node.py
@@ -4,19 +4,19 @@ from std_msgs.msg import String
 string=''
 def callback(data):
     global string
-    string=str(data.data)
+    string=str(data.data) #storing data got from the subscribed topic
 
 def publisher():
-    pub = rospy.Publisher('naayihba_maet', String, queue_size=10)
-    rospy.init_node('reversing_node', anonymous=True)
-    rospy.Subscriber("team_abhiyaan", String, callback)
+    pub = rospy.Publisher('naayihba_maet', String, queue_size=10) #publishing topic
+    rospy.init_node('reversing_node', anonymous=True) #initializing node
+    rospy.Subscriber("team_abhiyaan", String, callback) #subscribing to required topic
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
-    	reverse=''
+    	reverse=''          #reversing the message received
     	for i in string.split():
     		reverse+=i[::-1]+' '
-    	rospy.loginfo(reverse)
-    	pub.publish(reverse)
+    	rospy.loginfo(reverse) #printing reverse message to terminal
+    	pub.publish(reverse) #publishing new message to required topic
     	rate.sleep()
 if __name__ == '__main__':
     try:
