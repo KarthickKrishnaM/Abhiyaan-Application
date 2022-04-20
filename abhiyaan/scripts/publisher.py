c96dff6e359a247645812619b7afa4bf28e9c887
Karthick Krishna M <73303030+KarthickKrishnaM@users.noreply.github.com>
2022-04-20 21:18:26 +0530

    Update publisher.py

--- abhiyaan/scripts/publisher.py
+++ abhiyaan/scripts/publisher.py
@@ -3,12 +3,12 @@ import rospy
 from std_msgs.msg import String
 
 def publisher():
-    pub = rospy.Publisher('team_abhiyaan', String, queue_size=10)
-    rospy.init_node('publisher', anonymous=True)
+    pub = rospy.Publisher('team_abhiyaan', String, queue_size=10) #publishing to required topic
+    rospy.init_node('publisher', anonymous=True)    #initializing node
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         string = "Team Abhiyaan rocks:"
-        rospy.loginfo(string)
+        rospy.loginfo(string) #printing published data to terminal
         pub.publish(string)
         rate.sleep()
 
