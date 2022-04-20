1c70d69fca657e7fc287a27e8d29f73146073651
Karthick Krishna M <73303030+KarthickKrishnaM@users.noreply.github.com>
2022-04-20 21:26:17 +0530

    Update B.1.1.py

--- B.1.1.py
+++ B.1.1.py
@@ -1,15 +1,15 @@
 l=input().split()
 m,n=int(l[0]),int(l[1])
-k=input()
+k=input() #getting k as a string
 count=0
 index=[]
 for i in range(m):
-    r=input().split()
-    if count==0:
+    r=input().split() #input rows one by one
+    if count==0: #if k is not found yet, then we search for it
         for j in range(n):
-            if r[j]==k:
-                count=1
-                index.append(i)
+            if r[j]==k: #if k is found in the added row
+                count=1 
+                index.append(i) #update index with position of k, we dont have to check for k from the next row.
                 index.append(j)
                 break
 if count==0:
